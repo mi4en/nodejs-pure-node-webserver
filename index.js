e6f5abf4db1e59a2a688e1451dfe585d2717d615
mi4en <marionikolov87@gmail.com>
2019-07-17 22:38:16 +0300

    add very simple server serving few files;

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
-const Person = require('./person');
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
-logger.on('message', data => console.log('Called Listener: ', data));
+const server = http.createServer((req, res) => {
+	// Build dynamic file path
+	let filePath = path.join(
+		__dirname,
+		'public',
+		req.url === '/' ? 'index.html' : req.url
+	);
+	console.log(filePath);
 
-logger.log('hello world');
-logger.log('bye');
-logger.log('just something');
+	// Extension of file
+	let extname = path.extname(filePath);
+	console.log(extname);
 
-const person1 = new Person('Scorpien', 1234);
-person1.greeting();
-console.log(person1);
+	// Initial content type
+	let contentType = 'text/html';
+
+	// Check ext and set content type
+	switch (extname) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+		case '.json':
+			contentType = 'application/json';
+			break;
+		case '.png':
+			contentType = 'image/png';
+			break;
+		case '.jpg':
+			contentType = 'image/jpg';
+			break;
+	}
+
+	// Check if contentType is text/html but no .html file extension
+	if (contentType == 'text/html' && extname == '') filePath += '.html';
+
+	// log the filePath
+	console.log(filePath);
+
+	// Read file
+	fs.readFile(filePath, (err, content) => {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				// ENOENT === Page not found
+				fs.readFile(
+					path.join(__dirname, 'public', '404.html'),
+					(err, content) => {
+						res.writeHead(200, { 'Content-Type': 'text/html' });
+						res.end(content, 'utf8');
+					}
+				);
+			} else {
+				// Some server error
+				res.writeHead(500);
+				res.end(`Server Error: ${err.code}`);
+			}
+		} else {
+			// Success
+			res.writeHead(200, { 'Content-Type': contentType });
+			res.end(content, 'utf8');
+		}
+	});
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`server running on por ${PORT}`));
--- logger.js
+++ logger.js
@@ -1,5 +1,6 @@
 const EventEmitter = require('events');
 const uuid = require('uuid');
+const Person = require('./person');
 
 console.log(uuid.v4());
 
@@ -9,5 +10,16 @@ class Logger extends EventEmitter {
 		this.emit('message', { id: uuid.v4(), msg });
 	}
 }
+
+const logger = new Logger();
+logger.on('message', data => console.log('Called Listener: ', data));
+
+logger.log('hello world');
+logger.log('bye');
+logger.log('just something');
+
+const person1 = new Person('Scorpien', 1234);
+person1.greeting();
+console.log(person1);
 
 module.exports = Logger;
